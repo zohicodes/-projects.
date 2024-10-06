e6d2819a6028ebbe38f877eefa40f3a124ef3fa1
codedzoya <146192465+zohicodes@users.noreply.github.com>
2024-10-06 20:42:10 +0800

    Add files via upload

---
+++ RFID_ReaderWriter.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
+
+void setup() {
+  Serial.begin(9600);   
+  SPI.begin();          
+  mfrc522.PCD_Init();   
+  Serial.println("Place your card near the reader...");
+}
+
+void loop() {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Show UID on serial monitor
+  Serial.print("UID tag: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+
+  // Halt PICC
+  mfrc522.PICC_HaltA();
+}
+
